20e652013870a5392f244bc6548a1f08e8af1e90
ikrizanec <85575300+ikrizanec@users.noreply.github.com>
2024-01-08 21:16:30 +0100

    Dodani buttoni.

--- Sudoku/Sudoku/Form1.Designer.cs
+++ Sudoku/Sudoku/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Sudoku
+namespace Sudoku
 {
     partial class Form1
     {
@@ -34,6 +34,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.buttonAbout = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -43,7 +44,7 @@
             this.label1.BackColor = System.Drawing.Color.Black;
             this.label1.Font = new System.Drawing.Font("Algerian", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.DarkRed;
-            this.label1.Location = new System.Drawing.Point(733, 48);
+            this.label1.Location = new System.Drawing.Point(795, 102);
             this.label1.Margin = new System.Windows.Forms.Padding(38, 41, 38, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(432, 89);
@@ -62,7 +63,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Algerian", 22F);
             this.button1.ForeColor = System.Drawing.Color.DarkRed;
-            this.button1.Location = new System.Drawing.Point(9, 327);
+            this.button1.Location = new System.Drawing.Point(9, 381);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(251, 124);
@@ -85,7 +86,7 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Algerian", 22F);
             this.button2.ForeColor = System.Drawing.Color.DarkRed;
-            this.button2.Location = new System.Drawing.Point(9, 509);
+            this.button2.Location = new System.Drawing.Point(12, 719);
             this.button2.Margin = new System.Windows.Forms.Padding(0);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(251, 124);
@@ -104,7 +105,7 @@
             this.button3.Font = new System.Drawing.Font("Algerian", 12F);
             this.button3.ForeColor = System.Drawing.Color.DarkRed;
             this.button3.Location = new System.Drawing.Point(65, 21);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(59, 36);
             this.button3.TabIndex = 5;
@@ -127,20 +128,44 @@
             this.label2.TabIndex = 6;
             this.label2.Text = "Sound";
             // 
+            // buttonAbout
+            // 
+            this.buttonAbout.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonAbout.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonAbout.BackColor = System.Drawing.Color.Black;
+            this.buttonAbout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonAbout.FlatAppearance.BorderColor = System.Drawing.Color.DarkRed;
+            this.buttonAbout.FlatAppearance.BorderSize = 5;
+            this.buttonAbout.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.buttonAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAbout.Font = new System.Drawing.Font("Algerian", 22F);
+            this.buttonAbout.ForeColor = System.Drawing.Color.DarkRed;
+            this.buttonAbout.Location = new System.Drawing.Point(9, 555);
+            this.buttonAbout.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.Size = new System.Drawing.Size(251, 124);
+            this.buttonAbout.TabIndex = 7;
+            this.buttonAbout.Text = "ABOUT";
+            this.buttonAbout.UseVisualStyleBackColor = false;
+            this.buttonAbout.Click += new System.EventHandler(this.buttonAbout_Click);
+            this.buttonAbout.MouseEnter += new System.EventHandler(this.gumb_MouseEnter);
+            this.buttonAbout.MouseLeave += new System.EventHandler(this.gumb_MouseLeave);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Sudoku.Properties.Resources.pozadina;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1134, 680);
+            this.ClientSize = new System.Drawing.Size(1259, 789);
+            this.Controls.Add(this.buttonAbout);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SUDOKU";
@@ -156,6 +181,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonAbout;
     }
 }
 
